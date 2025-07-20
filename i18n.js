2072d9b1f3ce8dfbd5b4a567006737080d6aab79
Nathanb-ecam <nath.buchin@gmail.com>
2025-07-20 17:19:14 +0200

    locale domain

--- i18n.js
+++ i18n.js
@@ -1,5 +1,19 @@
 export default {
   locales: ['en', 'fr', 'es'],
   defaultLocale: 'es',
-  localeDetection: true
+  localeDetection: true,
+  domains: [
+  {
+    domain: 'labeuromed.eu',
+    defaultLocale: 'es',
+  },
+  {
+    domain: 'labeuromed.fr',
+    defaultLocale: 'fr',
+  },
+  {
+    domain: 'labeuromed.en',
+    defaultLocale: 'en',
+  },
+],
 }; 
