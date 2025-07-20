d749b74507f88de37470b7b1077e07b4691f2115
Nathanb-ecam <nath.buchin@gmail.com>
2025-07-20 15:05:30 +0200

    fixed layout import file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@
 // }
 
 
-import '../globals.css';
+import './globals.css';
 import {NextIntlClientProvider, hasLocale} from 'next-intl';
 import {notFound} from 'next/navigation';
 import {routing} from '@/i18n/routing';
