dfc96728afd3af54c0c6eb8aace087db1bf9a3dd
Nathanb-ecam <nath.buchin@gmail.com>
2025-07-20 15:02:50 +0200

    moved locale layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,53 @@
-// app/layout.tsx
+// import '../globals.css';
+// import type { Metadata } from 'next';
+// import { NextIntlClientProvider } from 'next-intl';
+// import { Inter } from 'next/font/google';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+// const inter = Inter({ subsets: ['latin'] });
+
+// export const metadata: Metadata = {};
+
+// export default async function RootLayout({ children, params }: {
+//   children: React.ReactNode;
+//   params: { locale: string };
+// }) {
+//   const { locale } = await params;
+//   return (
+//     <html lang={locale}>
+//       <head />
+//       <body className={inter.className}>
+//         <NextIntlClientProvider locale={locale}>
+//           {children}
+//         </NextIntlClientProvider>
+//       </body>
+//     </html>
+//   );
+// }
+
+
+import '../globals.css';
+import {NextIntlClientProvider, hasLocale} from 'next-intl';
+import {notFound} from 'next/navigation';
+import {routing} from '@/i18n/routing';
+ 
+export default async function LocaleLayout({
+  children,
+  params
+}: {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>
+}) {
+  // Ensure that the incoming `locale` is valid
+  const {locale} = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+ 
   return (
-    // <html>
-    //   <body>{children}</body>
-    // </html>
-    <>
-        {children}
-    </>
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      </body>
+    </html>
   );
 }
